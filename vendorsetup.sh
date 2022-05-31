2bb03a0b64eaa047c534a0370e35662ba714976b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-31 18:44:53 +0200

    msm8953_64: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: vivo Model: vivo 1727 Script version: 2.3.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_msm8953_64-user
+add_lunch_combo omni_msm8953_64-userdebug
+add_lunch_combo omni_msm8953_64-eng
